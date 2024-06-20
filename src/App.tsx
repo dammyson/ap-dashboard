bf535bb38f8f445444c7bee14e63dff8ca83be64
Achugo Ebuka <achugoebuka1@gmail.com>
2024-06-20 23:37:19 +0100

    Button component implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Email, Padlock, SlashedEye } from './components/svg/auth/AuthIcons';
 import { Input, InputState } from './components/input';
+import { BorderRadius, Button, ButtonSize } from './components/button';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,26 +9,35 @@ function App() {
 
   return (
     <div className='grid p-10 border-2 place-items-center rounded-xl bg-light-primary-transparent'>
-      <Input
-        state={hasError ? InputState.ERROR : InputState.Success}
-        label='Email'
-        placeHolder='Email Address'
-        inputSize='large'
-        leadingIcon={<Email />}
-        isCurved
-        helper={'Enter the correct email address.'}
-      />
+      <div className='max-w-[700px] mx-auto'>
+        <Input
+          state={hasError ? InputState.ERROR : InputState.Success}
+          label='Email'
+          placeHolder='Email Address'
+          inputSize='large'
+          leadingIcon={<Email />}
+          isCurved
+          helper={'Enter the correct email address.'}
+        />
 
-      <Input
-        state={InputState.NORMAL}
-        label='Email'
-        placeHolder='Password'
-        inputSize='large'
-        leadingIcon={<Padlock />}
-        trailingIcon={<SlashedEye />}
-        isCurved
-        type='password'
-      />
+        <Input
+          state={InputState.NORMAL}
+          label='Email'
+          placeHolder='Password'
+          inputSize='large'
+          leadingIcon={<Padlock />}
+          trailingIcon={<SlashedEye />}
+          isCurved
+          type='password'
+        />
+        <Button
+          buttonText='Log in'
+          radius={BorderRadius.Large}
+          className='mt-8 font-bold'
+          size={ButtonSize.Large}
+          mode='solid'
+        />
+      </div>
     </div>
   );
 }
