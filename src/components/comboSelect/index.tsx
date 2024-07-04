644fe0bb80ff01142d7119ee70e66753232e9a13
Augustine <mayakiaugustine0@gmail.com>
2024-07-04 05:24:01 +0100

    Implement custom role dorpdown select

---
+++ src/components/comboSelect/index.tsx
@@ -0,0 +1,108 @@
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+  Field,
+  Label,
+} from '@headlessui/react';
+import clsx from 'clsx';
+import React, {
+  PropsWithChildren,
+  ReactNode,
+  useEffect,
+  useState,
+  useRef,
+} from 'react';
+
+export enum SelectType {
+  SELECT = 'select',
+  COMOB_SELECT = 'combo_select',
+}
+
+interface SelectProps {
+  selectType: SelectType;
+  label: string;
+  className?: string;
+  hasBorder?: Boolean;
+  traillingIcon?: ReactNode;
+  isCurved?: Boolean;
+  selectedRole: string;
+}
+
+export function Comboselect({
+  selectType,
+  label,
+  children,
+  className,
+  hasBorder,
+  isCurved,
+  selectedRole = 'Role..',
+  traillingIcon,
+}: PropsWithChildren<SelectProps>) {
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  let divRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let handler = (e: MouseEvent) => {
+      if (isActive && !divRef.current?.contains(e.target as Node))
+        setIsActive(false);
+    };
+
+    document.addEventListener('click', handler);
+
+    return () => {
+      document.removeEventListener('click', handler);
+    };
+  });
+
+  return (
+    <Field className='relative'>
+      {label && <Label className={'mb-1 inline-block'}>{label}</Label>}
+      <div className='relative'>
+        {selectType === SelectType.SELECT ? (
+          <>
+            <div>
+              <div
+                ref={divRef}
+                onClick={() => {
+                  setIsActive(!isActive);
+                }}
+                className={clsx(
+                  traillingIcon ? 'p4-4' : '',
+                  hasBorder ? 'border-light-blue-50' : 'border-transparent',
+                  isCurved ? 'rounded-[50px]' : 'rounded-[8px]',
+                  className,
+                  'relative w-full h-[65px] px-4 py-3 cursor-pointer border-[1px] border-[#BBCAE2] hover:border-[#96a3b7] shadow-md flex items-center',
+                )}
+              >
+                {selectedRole}
+                {traillingIcon && (
+                  <span className='absolute right-6 cursor-pointer top-1/2 -translate-y-3'>
+                    {traillingIcon}
+                  </span>
+                )}
+              </div>
+
+              {isActive && (
+                <div className='max-h-40 overflow-auto no-scrollbar bg-primary-white rounded-2xl p-4-600 mt-2 py-2 border-[1px] border-[#BBCAE2]  shadow-xl'>
+                  {React.Children.map(children, (child) =>
+                    React.cloneElement(child as React.ReactElement<any>, {
+                      className: clsx(
+                        'text-xl cursor-pointer px-4 py-2 hover:bg-[#f4f4f4]',
+                        (child as React.ReactElement<any>).props.className,
+                      ),
+                    }),
+                  )}
+                </div>
+              )}
+            </div>
+          </>
+        ) : (
+          <Combobox></Combobox>
+        )}
+      </div>
+    </Field>
+  );
+}
--- src/components/profileInputField/index.tsx
+++ src/components/profileInputField/index.tsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
+import { Comboselect as RoleSelect, SelectType } from '../comboSelect';
 import { Input } from '../input';
+import clsx from 'clsx';
+import { DropDownArrow } from '../svg/settings/Settings';
 
 interface ProfileInput {
   label: string;
   id: string;
 }
-
 type ProfileInputs = ProfileInput[];
+
+interface RoleOption {
+  title: string;
+  id: string;
+}
+type RoleOptions = RoleOption[];
 
 export const ProfileInputField = () => {
   const profileInputs = [
@@ -16,10 +25,45 @@ export const ProfileInputField = () => {
     { label: 'Role', id: 'role' },
   ] as ProfileInputs;
 
+  const roleOptions = [
+    { title: 'Admin', id: 'admin' },
+    { title: 'HR', id: 'hr' },
+    { title: 'Trainer', id: 'trainer' },
+  ] as RoleOptions;
+
+  const [selectedRole, setSelectedRole] = useState<string>('' || 'Role');
   return (
     <div className='grid grid-cols-[minmax(250px,569px)_minmax(250px,569px)] gap-y-10 gap-32 justify-between py-6'>
       {profileInputs.map((item) => {
-        return (
+        return item.label === 'Role' ? (
+          <div
+            key={item.id}
+            className='text-light-grey-200 font-medium text-xl max-w-[569px] pr-8'
+          >
+            <RoleSelect
+              label='Role'
+              selectType={SelectType.SELECT}
+              hasBorder
+              isCurved
+              traillingIcon={<DropDownArrow />}
+              selectedRole={selectedRole}
+            >
+              {roleOptions.map((option) => (
+                <div
+                  className={clsx(
+                    selectedRole === option.title
+                      ? 'bg-[#e0e0e0] hover:bg-[#e0e0e0]'
+                      : '',
+                  )}
+                  key={option.id}
+                  onClick={() => setSelectedRole(option.title)}
+                >
+                  {option.title}
+                </div>
+              ))}
+            </RoleSelect>
+          </div>
+        ) : (
           <div
             key={item.id}
             className='text-light-grey-200 font-medium text-xl max-w-[569px]'
@@ -28,7 +72,7 @@ export const ProfileInputField = () => {
               label={item.label}
               isCurved
               hasBorder
-              className='drop-shadow-none text-xl border-[#BBCAE1] hover:border-[#a1afc4] h-[65px]'
+              className='drop-shadow-none text-xl border-[#BBCAE3] hover:border-[#8793a4] h-[65px]'
             />
           </div>
         );
