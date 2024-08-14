65ae93d6ab76cc439a2045e9d31539e98fd47dc2
Augustine <mayakiaugustine0@gmail.com>
2024-08-14 09:48:59 +0100

    add: overflow-x revenue resouces graph on customer page

--- src/pages/customer/viewCustomer/index.tsx
+++ src/pages/customer/viewCustomer/index.tsx
@@ -158,7 +158,7 @@ function ViewCustomer() {
                 trailingIcon1={<Filter />}
                 mainClass='h-full max-h-[513px]'
               >
-                <div className='flex items-center gap-1 640:gap-2 mb-12'>
+                <div className='flex items-center gap-1 640:gap-2 mb-12 overflow-x-auto'>
                   {tabs.map((tab, index) => (
                     <div
                       onClick={() => setActiveTab(tab)}
