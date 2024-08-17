66f20cfa5abad3b45fd66a9283fb5108646db44c
Augustine <mayakiaugustine0@gmail.com>
2024-08-17 05:50:43 +0100

    mobile ui implementation of customer page

--- src/components/customSelect/index.tsx
+++ src/components/customSelect/index.tsx
@@ -68,24 +68,25 @@ export function CustomSelect({
                 hasBorder ? 'border-light-blue-50' : 'border-transparent',
                 isCurved ? 'rounded-[50px]' : 'rounded-[8px]',
                 className,
-                'bg-primary-white relative w-full h-[65px] px-4 py-3 cursor-pointer border-[1px] !border-light-blue-50 hover:!border-[#acbbd0] shadow-md flex items-center',
+                'bg-primary-white relative w-full h-[65px] px-4 py-3 cursor-pointer border-[1px] !border-light-blue-50 hover:!border-[#acbbd0] shadow-md flex items-center  text-nowrap text-sm 560:text-base 880:text-lg 1024:text-xl',
               )}
             >
               {selectedRole}
               {trailingIcon && (
-                <span className='absolute right-6 cursor-pointer top-1/2 -translate-y-3'>
+                <span className='absolute right-6 cursor-pointer top-1/2 -translate-y-3 bg-white'>
                   {trailingIcon}
                 </span>
               )}
             </div>
             {isActive && (
-              <div className=' absolute z-50 w-full max-h-40 overflow-auto hidden-scrollbar bg-primary-white rounded-2xl p-4-600 mt-2 py-2 border-[1px] border-[#BBCAE2]  shadow-xl'>
+              <div className='text-sm absolute z-50 w-full max-h-40 overflow-auto hidden-scrollbar bg-primary-white rounded-2xl p-4-600 mt-2 py-2 border-[1px] border-[#BBCAE2] shadow-xl'>
                 {options.map((option) => (
                   <div
                     className={clsx(
                       selectedRole === option.label
-                        ? 'bg-[#e0e0e0] cursor-pointer px-4 text-xl py-2'
-                        : 'px-4 py-2 text-xl hover:bg-[#f4f4f4]',
+                        ? 'bg-[#e0e0e0] cursor-pointer'
+                        : ' hover:bg-[#f4f4f4]',
+                      'px-4 py-2 text-base 880:text-lg 1024:text-xl',
                     )}
                     key={option.value}
                     onClick={() => (onSelect ? onSelect(option.label) : null)}
--- src/pages/customer/index.tsx
+++ src/pages/customer/index.tsx
@@ -69,14 +69,15 @@ function Customer() {
               size={SizeType.LARGE}
               cancelIcon={<Cancel />}
               onClick={() => setAwardPoints(false)}
+              className=''
             >
-              <div className='flex  flex-col items-center justify-center max-w-[633px] w-full py-5'>
-                <h3 className='text-light-primary-deep_black text-[32px] font-medium mb-10'>
+              <div className='flex flex-col items-center justify-center w-full 768:w-4/5 960:w-[70%] 1240:py-5'>
+                <h3 className='text-light-primary-deep_black text-lg 560:text-xl 768:text-2xl 960:text-[28px] 1240:text-[32px] font-medium mb-4 768:mb-6 1400:mb-10 pt-5 960:pt-0'>
                   Manually award points to user
                 </h3>
                 <div className='w-11/12'>
-                  <div className='mb-10 text-light-grey-600'>
-                    <p className='text-left text-xl font-medium pb-4'>
+                  <div className='mb-3 768:mb-5 1400:mb-10 text-light-grey-600'>
+                    <p className='text-left text-base 768:text-lg 960:text-xl font-medium 960:pb-2 1400:pb-4'>
                       Points to award
                     </p>
                     <CustomSelect
@@ -88,10 +89,11 @@ function Customer() {
                       trailingIcon={<DropDownArrow />}
                       selectedRole={selectedPointOption}
                       onSelect={(option) => setSelectedPointOption(option)}
+                      className='!h-[50px] 1024:!h-[57px] 1300:!min-h-[65px]'
                     />
                   </div>
-                  <div className='mb-5 text-light-grey-600'>
-                    <p className='text-left text-xl  font-medium pb-4'>
+                  <div className='768:mb-5 text-light-grey-600'>
+                    <p className='text-left text-base 768:text-lg 960:text-xl font-medium 960:pb-2 1400:pb-4'>
                       Reason for awarding points
                     </p>
                     <CustomSelect
@@ -103,16 +105,17 @@ function Customer() {
                       trailingIcon={<DropDownArrow />}
                       selectedRole={selectedReason}
                       onSelect={(option) => setSelectedReason(option)}
+                      className='!h-[50px] 1024:!h-[57px] 1300:!min-h-[65px]'
                     />
                   </div>
                 </div>
 
-                <ul className='w-full list-disc list-inside text-left pt-2 pb-20'>
-                  <li className='text-[16px] text-light-grey-600'>
+                <ul className='w-full list-disc list-inside text-left pt-2 pb-8 1240:pb-10 1400:pb-20'>
+                  <li className='text-xs 1024:text-base text-light-grey-600'>
                     <span className='font-medium'>Note:</span> Manually awarded
                     points will be reflected in the user's account immediately.
                   </li>
-                  <li className='text-[16px] text-light-grey-600'>
+                  <li className='text-xs 1024:text-base text-light-grey-600'>
                     Please ensure the points amount is accurate and the reason
                     is clear.
                   </li>
@@ -124,7 +127,7 @@ function Customer() {
                     radius={BorderRadius.Large}
                     buttonText='Award points'
                     onClick={() => {}}
-                    className='!font-semibold !text-2xl'
+                    className='!font-semibold 768:!text-xl 1240:!text-2xl !min-h-[50px] 1024:!min-h-[57px] 1300:!min-h-[65px]'
                   />
                   <Button
                     size={ButtonSize.Large}
@@ -132,7 +135,7 @@ function Customer() {
                     mode='outlined'
                     buttonText='Cancel'
                     onClick={() => setAwardPoints(false)}
-                    className='!font-semibold !text-2xl'
+                    className='!font-semibold 768:!text-xl 1240:!text-2xl !min-h-[50px] 1024:!min-h-[57px] 1300:!min-h-[65px]'
                   />
                 </div>
               </div>
