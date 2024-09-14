013b9a160692b03b537408d8a3beea7af007c9fc
Achugo Ebuka <achugoebuka1@gmail.com>
2024-09-15 00:25:49 +0100

    feat: protected and public routes

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,48 @@
+import { Routes, Route } from 'react-router-dom';
+import PublicRoutes from './PublicRoutes';
+import Login from '@/pages/auth/login/Login';
+import ForgotPassword from '@/pages/auth/forgotpassword/ForgotPassword';
+import ResetPassword from '@/pages/auth/resetpassword/ResetPassword';
+import Dashboard from '@/pages/dashboard';
+import Customer from '@/pages/customer';
+import ViewCustomer from '@/pages/customer/viewCustomer';
+import ActivityLog from '@/pages/activity-log';
+import Surveys from '@/pages/surveys';
+import CustomerFeedback from '@/pages/surveys/customerFeedback';
+import EditSurvey from '@/pages/surveys/editSurvey';
+import CreateSurvey from '@/pages/surveys/createSurvery';
+import Settings from '@/pages/settings';
+import { useUser } from '@/context/AppContext';
+import ProtectedRoutes from './PrivateRoutes';
+
+const AppRoutes = () => {
+  const { token } = useUser();
+
+  return (
+    <Routes>
+      <Route element={<PublicRoutes isAuthenticated={token ? true : false} />}>
+        <Route path='/' element={<Login />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+      </Route>
+      <Route
+        element={<ProtectedRoutes isAuthenticated={token ? true : false} />}
+      >
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/customer' element={<Customer />} />
+        <Route
+          path='/customer-view/:titleId/:nameId'
+          element={<ViewCustomer />}
+        />
+        <Route path='/activity-log' element={<ActivityLog />} />
+        <Route path='/surveys' element={<Surveys />} />
+        <Route path='/surveys-feedback/:id' element={<CustomerFeedback />} />
+        <Route path='/surveys-edit/:id' element={<EditSurvey />} />
+        <Route path='/surveys-create/:id' element={<CreateSurvey />} />
+        <Route path='/settings' element={<Settings />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
