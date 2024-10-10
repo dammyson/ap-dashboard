687994894ab91c3a34badbea69c1e794417ab18c
Augustine <mayakiaugustine0@gmail.com>
2024-10-10 09:33:29 +0100

    implementation of offline ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,51 @@ import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './context/AppContext';
 import { Toaster } from 'sonner';
 import AppRoutes from './routes/AppRoutes';
+import { useEffect, useState } from 'react';
+import { NoConnection } from './components/noConnection/noConnection';
 
 const App = () => {
+  const [isOnline, setIsOnline] = useState<boolean>(navigator.onLine);
+
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []);
+
+  useEffect(() => {
+    let interval: ReturnType<typeof setInterval>;
+
+    if (!isOnline) {
+      interval = setInterval(() => {
+        setIsOnline(navigator.onLine);
+      }, 30000);
+    }
+
+    return () => clearInterval(interval);
+  }, [isOnline]);
+
   return (
     <div>
-      <UserProvider>
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
-      </UserProvider>
-      <Toaster richColors duration={3000} position='top-right' />
+      {!isOnline ? (
+        <NoConnection onClick={() => {}} />
+      ) : (
+        <>
+          <UserProvider>
+            <BrowserRouter>
+              <AppRoutes />
+            </BrowserRouter>
+          </UserProvider>
+          <Toaster richColors duration={3000} position='top-right' />
+        </>
+      )}
     </div>
   );
 };
