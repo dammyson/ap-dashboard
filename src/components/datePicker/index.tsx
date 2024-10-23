8735d7c80c4d9806d9055a0f60d9767b0fdc355a
Augustine <mayakiaugustine0@gmail.com>
2024-10-23 08:34:17 +0100

    fix date picker bug for selecting enddate before startdate

--- src/components/datePicker/index.tsx
+++ src/components/datePicker/index.tsx
@@ -3,6 +3,7 @@ import dayjs from 'dayjs';
 import { SetStateAction } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import { Calender } from '../svg/activityLog/ActivityLog';
+import { toast } from 'sonner';
 
 interface DateProps {
   startDate: Date | undefined;
@@ -17,22 +18,44 @@ export const CustomDatePicker = ({
   endDate,
   setEndDate,
 }: DateProps) => {
+  const handleStartDateChange = (date: Date | undefined) => {
+    setStartDate(date);
+
+    if (endDate && date && endDate < date) {
+      setEndDate(undefined);
+    }
+  };
+
+  const handleEndDateChange = (date: Date | undefined) => {
+    if (startDate && date && date < startDate) {
+      toast.warning('End date cannot be earlier than start date');
+      return;
+    }
+    setEndDate(date);
+  };
   return (
     <div className='flex flex-col 768:flex-row 768:items-center 768:justify-between gap-3 768:gap-8 mt-2.5 max-w-[760px]'>
       <div className='max-w-[350px] 768:max-w-[420px] w-full relative'>
         <DatePicker
-          value={startDate ? dayjs(startDate) : undefined}
-          onChange={(date) => (date ? setStartDate(date.toDate()) : undefined)}
+          value={startDate ? dayjs(startDate) : null}
+          onChange={(date) =>
+            date
+              ? handleStartDateChange(date.toDate())
+              : setStartDate(undefined)
+          }
           maxDate={dayjs()}
           placeholder='Start'
           format='DD-MM-YYYY'
           suffixIcon={<Calender />}
+          allowClear={true}
         />
       </div>
       <div className='max-w-[350px] 768:max-w-[420px] w-full relative'>
         <DatePicker
-          value={endDate ? dayjs(endDate) : undefined}
-          onChange={(date) => (date ? setEndDate(date.toDate()) : null)}
+          value={endDate ? dayjs(endDate) : null}
+          onChange={(date) =>
+            date ? handleEndDateChange(date.toDate()) : setEndDate(undefined)
+          }
           minDate={startDate ? dayjs(startDate) : undefined}
           maxDate={dayjs()}
           placeholder='End'
