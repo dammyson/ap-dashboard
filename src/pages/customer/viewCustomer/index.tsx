7a9254c46eb6fea9d1f6a289f273b1d5bc4d470b
Augustine <mayakiaugustine0@gmail.com>
2024-12-13 15:33:23 +0100

    created component for flight information

--- src/pages/customer/viewCustomer/index.tsx
+++ src/pages/customer/viewCustomer/index.tsx
@@ -1,16 +1,12 @@
 import { Card } from '@/components/card';
 import { Header } from '@/components/header';
 import { AppLayout } from '@/components/layout/AppLayout';
-import {
-  Flight,
-  SmallDropDown,
-  UsageTime,
-} from '@/components/svg/customer/Customer';
+import { SmallDropDown, UsageTime } from '@/components/svg/customer/Customer';
 import { Filter } from '@/components/svg/surveys/Surveys';
 import { numberShortener } from '@/utils';
 import clsx from 'clsx';
 import { useNavigate, useParams } from 'react-router';
-import { usageStats, UserFlightDetails } from './constants';
+import { usageStats } from './constants';
 import { useEffect, useRef, useState } from 'react';
 import { Chart } from '@/components/chart/Chart';
 import { useWindowSize } from '@/components/hooks/useWindowSize';
@@ -23,6 +19,7 @@ import { Table } from 'antd';
 import { useCustomerActivityLog } from '@/components/modules/customer/activityLog/tableColumns';
 import { graphOptions } from '@/constants/constants';
 import { useClickOutside } from '@/components/hooks/useClickOutside';
+import { FlightInfo } from '@/components/modules/customer/flightInformation/flightInformation';
 
 function ViewCustomer() {
   const navigate = useNavigate();
@@ -258,90 +255,7 @@ function ViewCustomer() {
             {fetching ? (
               <SkeletonLoader singleLoader />
             ) : (
-              <Card>
-                <div className='grid grid-cols-1 768:grid-cols-2 1024:flex 1024:items-start justify-items-center 1024:justify-between gap-3'>
-                  {UserFlightDetails.map((user, index) => (
-                    <div key={index}>
-                      <div className='text-center 1024:text-start'>
-                        <p className='text-light-grey-700 text-[16px] 768:text-xl 1240:text-lg 1400:text-x 1024:h-[56px] font-medium'>
-                          {user.title}
-                        </p>
-                        <p className='text-light-primary-black font-bold text-xl 768:text-2xl 880:text-[26px] 1240:text-[28px] 1400:text-[30px]'>
-                          {user.title === 'Travel preferences'
-                            ? 'Window Seat'
-                            : user.title === 'Last flight'
-                              ? `${dayjs(
-                                  customer?.last_flight.departure_time,
-                                ).format('YYYY-MM-DD')}`
-                              : user?.title === 'Upcoming flight'
-                                ? `${dayjs(
-                                    customer?.upcoming_flight.departure_time,
-                                  ).format('YYYY-MM-DD')}`
-                                : dayjs(customer?.user_date_of_reg).format(
-                                    'YYYY-MM-DD',
-                                  )}
-                        </p>
-                      </div>
-                      {user.title.includes('flight') && (
-                        <div
-                          className={clsx(
-                            user.title.includes('Last')
-                              ? 'text-light-blue-main flex items-center'
-                              : user.title.includes('Upcoming')
-                                ? 'text-light-secondary-orange'
-                                : '',
-                            'flex gap-1 360:gap-3',
-                          )}
-                        >
-                          <div>
-                            <p className='font-medium text-xl 360:text-2xl 480:text-[26px] 1240:text-[26px] 1400:text-[30px] '>
-                              {user.title === 'Last flight'
-                                ? customer?.last_flight.origin_city != ''
-                                  ? customer?.last_flight.origin_city
-                                  : customer?.last_flight.origin
-                                : customer?.upcoming_flight.origin_city != ''
-                                  ? customer?.upcoming_flight.origin_city
-                                  : customer?.upcoming_flight.origin}
-                            </p>
-                            <p className='text-[10px] 360:text-sm 480:text-[16px]'>
-                              {customer?.last_flight.origin}
-                            </p>
-                          </div>
-                          <div className='pt-[10px]'>
-                            <Flight
-                              color={clsx(
-                                user.title.includes('Last')
-                                  ? '#23539F'
-                                  : user.title.includes('Upcoming')
-                                    ? '#F09436'
-                                    : '',
-                              )}
-                              className='max-w-[130px] w-full'
-                            />
-                          </div>
-                          <div>
-                            <p className='font-medium text-xl 360:text-2xl 480:text-[26px] 1240:text-[26px] 1400:text-[30px] '>
-                              {user.title === 'Last flight'
-                                ? customer?.last_flight.destination_city != ''
-                                  ? customer?.last_flight.destination_city
-                                  : customer?.last_flight.destination
-                                : customer?.upcoming_flight.destination_city !=
-                                    ''
-                                  ? customer?.upcoming_flight.destination_city
-                                  : customer?.upcoming_flight.destination}
-                            </p>
-                            <p className='float-end text-[10px] 360:text-sm 480:text-[16px]'>
-                              {user.title === 'Last flight'
-                                ? customer?.last_flight.destination
-                                : customer?.upcoming_flight.destination}
-                            </p>
-                          </div>
-                        </div>
-                      )}
-                    </div>
-                  ))}
-                </div>
-              </Card>
+              <FlightInfo customer={customer} />
             )}
             {fetching ? (
               <SkeletonLoader singleLoader />
