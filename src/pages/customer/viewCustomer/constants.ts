777d89a9200164fef7b67049fdbc7aba62f1e8cb
Augustine <mayakiaugustine0@gmail.com>
2025-02-05 10:40:06 +0100

    removed unused constants

--- src/pages/customer/viewCustomer/constants.ts
+++ src/pages/customer/viewCustomer/constants.ts
@@ -19,26 +19,8 @@ export const usageStats = [
   },
 ];
 export const UserFlightDetails = [
-  { title: 'Date of registration', detail: '2024-05-28' },
-  { title: 'Travel preferences', detail: 'Window seat' },
-  { title: 'Last flight', detail: '2024-05-15' },
-  { title: 'Upcoming flight', detail: '2024-06-10' },
-];
-
-export const ActivityList = [
-  {
-    date: '2024-05-28',
-    activityType: 'Booking',
-    description: 'Booked flight to NYC, flight #1234',
-  },
-  {
-    date: '2024-06-10',
-    activityType: 'In-flight purchases',
-    description: 'Purchases from PIF',
-  },
-  {
-    date: '2024-06-10',
-    activityType: 'BookinReferralsg',
-    description: 'Referred new customer Jane Ayomide ',
-  },
+  { title: 'Date of registration' },
+  { title: 'Travel preferences' },
+  { title: 'Last flight' },
+  { title: 'Upcoming flight' },
 ];
